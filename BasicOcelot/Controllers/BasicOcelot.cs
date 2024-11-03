edc4dfc9dc4f9cf803d31a4ee798609ae584c7ee
ÖZGE <ZGE@LAPTOP-Q0UR50U0>
2024-11-03 22:41:03 +0300

    Proje dosyası ekle.

---
+++ BasicOcelot/Controllers/BasicOcelot.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicOcelotApi.Controllers
+{
+    public class BasicOcelot
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ValuesController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(new { Message = "Hello from ApiService!" });
+            }
+        }
+    }
+}
