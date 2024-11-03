edb8f4897c64f8ddbb95cd2291e20c7e0b0a20dd
OzgeOtles <ozgeotless@gmail.com>
2024-11-04 00:30:30 +0300

    basit ocelot yapısı hazırlandı

---
+++ ApiGateway/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+        }
+
+        public async void Configure(IApplicationBuilder app)
+        {
+            await app.UseOcelot();
+        }
+    }
+}
